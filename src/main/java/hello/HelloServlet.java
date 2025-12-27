6ed55f493fe9c8294f9c0cfc1bff702987334666
Darshansv17 <darshansv1711999@gmail.com>
2025-12-28 01:58:18 +0530

    Change greeting message in HelloServlet

--- src/main/java/hello/HelloServlet.java
+++ src/main/java/hello/HelloServlet.java
@@ -13,6 +13,6 @@ public class HelloServlet extends HttpServlet {
             throws ServletException, IOException {
 
         resp.setContentType("text/plain");
-        resp.getWriter().println("i love you KEERTHANA");
+        resp.getWriter().println("DARSHAN SV");
     }
 }
